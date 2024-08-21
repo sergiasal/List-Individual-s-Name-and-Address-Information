400ea0d96dfd4398391ef9d0fb5f22943cb90b26
sergiasal <sergiasal@gmail.com>
2024-08-20 22:14:34 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+git init
+git add Main.java
+git commit -m "Add Java application to print fictional person details"
+git branch -M main
+git remote add origin <your-repository-URL>
+git push -u origin main
